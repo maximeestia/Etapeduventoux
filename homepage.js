1ab1f680320366a23a861d395507dd82e444b2a9
majmaj52 <78183812+majmaj52@users.noreply.github.com>
2021-02-03 18:05:07 +0100

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,14 @@
+ScrollOut({
+    cssProps: true,
+    threshold: 0.2
+});
+
+
+//
+
+$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+
+$("#menu-icon").on("click", function() {
+    $("nav").slideToggle();
+    $(this).toggleClass("active");
+});
